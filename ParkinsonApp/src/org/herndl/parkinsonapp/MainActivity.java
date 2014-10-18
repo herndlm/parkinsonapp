c0ce2f636bbad45149d9ceed78a043e4d8efa731
martin.herndl <martin.herndl@3b3d3c4a-c70a-34b6-de48-a2ab4d55614c>
2014-10-18 16:07:12 +0000

    backup commit

--- ParkinsonApp/src/org/herndl/parkinsonapp/MainActivity.java
+++ ParkinsonApp/src/org/herndl/parkinsonapp/MainActivity.java
@@ -22,7 +22,6 @@ public class MainActivity extends ActionBarActivity implements ActionBar.TabList
      */
     AppSectionsPagerAdapter mAppSectionsPagerAdapter;
 	
-	public final static String EXTRA_MESSAGE = "org.herndl.parkinsonapp.MESSAGE";
 	private ViewPager mViewPager;
 
     @Override
@@ -142,7 +141,7 @@ public class MainActivity extends ActionBarActivity implements ActionBar.TabList
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
-        if (id == R.id.action_settings) {
+        if (id == R.id.action_about) {
             return true;
         }
         else if (id == R.id.action_search) {
