431764b76bdd6b966139c79a11e56e9a0604de0a
martin.herndl <martin.herndl@3b3d3c4a-c70a-34b6-de48-a2ab4d55614c>
2014-10-22 09:32:37 +0000

    MedReminderEntity hashCode for notification id checks

--- ParkinsonApp/src/org/herndl/parkinsonapp/med/MedReminderEntity.java
+++ ParkinsonApp/src/org/herndl/parkinsonapp/med/MedReminderEntity.java
@@ -48,4 +48,49 @@ public class MedReminderEntity extends SugarRecord<MedReminderEntity> {
 		}
 	}
 
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((dose == null) ? 0 : dose.hashCode());
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		result = prime * result
+				+ ((remind_hour == null) ? 0 : remind_hour.hashCode());
+		result = prime * result
+				+ ((remind_minute == null) ? 0 : remind_minute.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		MedReminderEntity other = (MedReminderEntity) obj;
+		if (dose == null) {
+			if (other.dose != null)
+				return false;
+		} else if (!dose.equals(other.dose))
+			return false;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+		if (remind_hour == null) {
+			if (other.remind_hour != null)
+				return false;
+		} else if (!remind_hour.equals(other.remind_hour))
+			return false;
+		if (remind_minute == null) {
+			if (other.remind_minute != null)
+				return false;
+		} else if (!remind_minute.equals(other.remind_minute))
+			return false;
+		return true;
+	}
+
 }
