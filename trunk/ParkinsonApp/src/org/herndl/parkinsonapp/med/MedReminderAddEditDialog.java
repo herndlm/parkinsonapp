2106d0d5446211be1d51e5e12566c85aa913a0d3
martin.herndl <martin.herndl@3b3d3c4a-c70a-34b6-de48-a2ab4d55614c>
2014-10-22 16:48:49 +0000

    med reminder edit functionality

---
+++ trunk/ParkinsonApp/src/org/herndl/parkinsonapp/med/MedReminderAddEditDialog.java
@@ -0,0 +1,152 @@
+package org.herndl.parkinsonapp.med;
+
+import org.herndl.parkinsonapp.R;
+
+import android.annotation.SuppressLint;
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.support.v4.app.DialogFragment;
+import android.text.format.DateFormat;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TimePicker;
+import android.widget.Toast;
+
+public class MedReminderAddEditDialog extends DialogFragment {
+	private String dialogTitle;
+	private int mode;
+	private MedReminderEntity medOld;
+	private int medOldListPos;
+
+	private static View view;
+	private static EditText input_med_name;
+	private static EditText input_med_dose;
+	private static TimePicker input_med_time;
+
+	public static class MedReminderAddEditDialogMode {
+		public static final int ADD = 1;
+		public static final int EDIT = 2;
+	}
+
+	public MedReminderAddEditDialog(String dialogTitle, int mode,
+			MedReminderEntity medOld, int medOldListPos) {
+		super();
+		this.dialogTitle = dialogTitle;
+		this.mode = mode;
+		this.medOld = medOld;
+		this.medOldListPos = medOldListPos;
+	}
+
+	@SuppressLint("InflateParams")
+	@Override
+	public Dialog onCreateDialog(Bundle savedInstanceState) {
+		super.onCreateDialog(savedInstanceState);
+		setRetainInstance(true);
+
+		AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+		LayoutInflater inflater = getActivity().getLayoutInflater();
+		view = inflater.inflate(R.layout.dialog_med_reminder_add_edit, null);
+
+		// get and set all input elements for later actions
+		getAllInputElements();
+
+		// adapt time picker to locale of the user
+		input_med_time
+				.setIs24HourView(DateFormat.is24HourFormat(getActivity()));
+
+		// prefill input values
+		prefillValues();
+
+		builder.setView(view);
+
+		builder.setTitle(dialogTitle)
+				// ok button handling
+				.setPositiveButton(android.R.string.ok,
+						new DialogInterface.OnClickListener() {
+							public void onClick(DialogInterface dialog, int id) {
+								// read user input
+								String med_name = input_med_name.getText()
+										.toString().trim();
+								String med_dose = input_med_dose.getText()
+										.toString().trim();
+
+								// check inputs (.isEquals is API level 9,
+								// we are targeting level 8 here)
+								if (med_name.equals("")) {
+									Toast.makeText(
+											getActivity()
+													.getApplicationContext(),
+											R.string.med_reminder_add_name_required,
+											Toast.LENGTH_SHORT).show();
+									return;
+								}
+								if (med_dose.equals("")) {
+									Toast.makeText(
+											getActivity()
+													.getApplicationContext(),
+											R.string.med_reminder_add_dose_required,
+											Toast.LENGTH_SHORT).show();
+									return;
+								}
+
+								// parse dose from string
+								int med_dose_int = 0;
+								try {
+									med_dose_int = Integer.parseInt(med_dose);
+								} catch (NumberFormatException e) {
+									Log.w("MedReminderAddDialog:PositiveButton",
+											"can't parse int from string "
+													+ med_dose);
+								}
+
+								// create new MedReminderEntity
+								MedReminderEntity med = new MedReminderEntity(
+										med_name, med_dose_int, input_med_time
+												.getCurrentHour(),
+										input_med_time.getCurrentMinute());
+
+								// add to list and DB
+								if (mode == MedReminderAddEditDialogMode.ADD)
+									MedReminderFragment.addMedItem(med);
+								else if (mode == MedReminderAddEditDialogMode.EDIT)
+									MedReminderFragment.editMedItem(
+											medOldListPos, med);
+							}
+						})
+				// cancel button handling
+				.setNegativeButton(android.R.string.cancel,
+						new MedReminderFragment.DummyOnClickListener());
+		// create the AlertDialog object and return it
+		return builder.create();
+	}
+
+	private void getAllInputElements() {
+		input_med_name = (EditText) view.findViewById(R.id.add_med_name);
+		input_med_dose = (EditText) view.findViewById(R.id.add_med_dose);
+		input_med_time = (TimePicker) view.findViewById(R.id.add_med_time);
+	}
+
+	// sets the GUI values to the one of the old med object
+	private void prefillValues() {
+		if (medOld == null)
+			return;
+		input_med_name.setText(medOld.name);
+		input_med_dose.setText("" + medOld.dose);
+		input_med_time.setCurrentHour(medOld.remind_hour);
+		input_med_time.setCurrentMinute(medOld.remind_minute);
+	}
+
+	// handling of fragment recreation bug in support library
+	// see https://code.google.com/p/android/issues/detail?id=17423
+	// e.g. retains the dialog on rotations
+	@Override
+	public void onDestroyView() {
+		if (getDialog() != null && getRetainInstance())
+			getDialog().setDismissMessage(null);
+		super.onDestroyView();
+	}
+}
--- trunk/ParkinsonApp/src/org/herndl/parkinsonapp/med/MedReminderFragment.java
+++ trunk/ParkinsonApp/src/org/herndl/parkinsonapp/med/MedReminderFragment.java
@@ -5,35 +5,30 @@ import java.util.List;
 
 import org.herndl.parkinsonapp.R;
 import org.herndl.parkinsonapp.TaskHandler;
+import org.herndl.parkinsonapp.med.MedReminderAddEditDialog.MedReminderAddEditDialogMode;
 
-import android.annotation.SuppressLint;
 import android.app.AlertDialog;
-import android.app.Dialog;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.v4.app.DialogFragment;
 import android.support.v4.app.Fragment;
-import android.text.format.DateFormat;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemClickListener;
+import android.widget.AdapterView.OnItemLongClickListener;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
-import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.TextView;
-import android.widget.TimePicker;
-import android.widget.Toast;
 
 // the fragment which holds the med reminder data
 public class MedReminderFragment extends Fragment {
 
-	private List<MedReminderEntity> listMeds;
-	private ArrayAdapter<MedReminderEntity> adapterMeds;
+	public static List<MedReminderEntity> listMeds;
+	public static ArrayAdapter<MedReminderEntity> adapterMeds;
 
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -60,40 +55,51 @@ public class MedReminderFragment extends Fragment {
 		adapterMeds = new MedReminderAdapter(rootView.getContext(), listMeds);
 		listMedReminder.setAdapter(adapterMeds);
 
-		// onItemClick handler for med delete actions
+		// OnItemClickListener for med edit actions
 		listMedReminder.setOnItemClickListener(new OnItemClickListener() {
-			public void onItemClick(AdapterView<?> parent, final View view,
-					final int position, long id) {
-				final MedReminderEntity med = listMeds.get(position);
-
-				AlertDialog.Builder builder = new AlertDialog.Builder(
-						getActivity());
-				// dialog buttons
-				builder.setTitle(
-						String.format(
-								getResources().getString(
-										R.string.med_reminder_delete), med.name))
-						.setPositiveButton(android.R.string.ok,
-								new DialogInterface.OnClickListener() {
-									public void onClick(DialogInterface dialog,
-											int id) {
-										// remove med item from system
-										removeMedItem(position);
-									}
-								})
-						.setNegativeButton(android.R.string.cancel,
-								new DialogInterface.OnClickListener() {
-									public void onClick(DialogInterface dialog,
-											int id) {
-										// do nothing
-									}
-								});
-
-				// creat the dialog
-				AlertDialog dialog = builder.create();
-				dialog.show();
+			public void onItemClick(AdapterView<?> parent, View view,
+					int position, long id) {
+				DialogFragment dialog = new MedReminderAddEditDialog(
+						getString(R.string.med_reminder_edit_title),
+						MedReminderAddEditDialogMode.EDIT, listMeds
+								.get(position), position);
+				dialog.show(getFragmentManager(), "dialog_med_reminder_add");
 			}
 		});
+
+		// OnItemLongClickListener for med delete actions
+		listMedReminder
+				.setOnItemLongClickListener(new OnItemLongClickListener() {
+					public boolean onItemLongClick(AdapterView<?> parent,
+							View view, final int position, long id) {
+						AlertDialog.Builder builder = new AlertDialog.Builder(
+								getActivity());
+
+						// dialog buttons
+						builder.setTitle(
+								String.format(
+										getResources().getString(
+												R.string.med_reminder_delete),
+										listMeds.get(position).name))
+								.setPositiveButton(android.R.string.ok,
+										new DialogInterface.OnClickListener() {
+											public void onClick(
+													DialogInterface dialog,
+													int id) {
+												// remove med item from system
+												removeMedItem(position);
+											}
+										})
+								.setNegativeButton(
+										android.R.string.cancel,
+										new MedReminderFragment.DummyOnClickListener());
+
+						// create the dialog
+						AlertDialog dialog = builder.create();
+						dialog.show();
+						return true;
+					}
+				});
 
 		// show MedReminderAddDialog when clicking on add button
 		Button addButton = (Button) rootView
@@ -101,7 +107,9 @@ public class MedReminderFragment extends Fragment {
 		addButton.setOnClickListener(new View.OnClickListener() {
 			@Override
 			public void onClick(View v) {
-				DialogFragment dialog = new MedReminderAddDialog();
+				DialogFragment dialog = new MedReminderAddEditDialog(
+						getString(R.string.med_reminder_add_title),
+						MedReminderAddEditDialogMode.ADD, null, 0);
 				dialog.show(getFragmentManager(), "dialog_med_reminder_add");
 			}
 		});
@@ -109,8 +117,32 @@ public class MedReminderFragment extends Fragment {
 		return rootView;
 	}
 
+	// helper onClickListener class which does nothing, this is useful for
+	// cancel actions which should just close dialogs
+	public static class DummyOnClickListener implements
+			DialogInterface.OnClickListener {
+		@Override
+		public void onClick(DialogInterface dialog, int which) {
+			// do nothing
+		}
+	}
+
+	// helper methode for adding a med item
+	public static void addMedItem(MedReminderEntity med) {
+		// save entity in DB
+		med.save();
+		// add to med list
+		listMeds.add(med);
+		// sort after remind time with custom comparator
+		Collections.sort(listMeds, new MedReminderEntity.medComparator());
+		// set reminder via TaskHandler
+		TaskHandler.setMedAlarm(med);
+		// update list view via adapter
+		adapterMeds.notifyDataSetChanged();
+	}
+
 	// helper methode for removing a med item
-	public void removeMedItem(int position) {
+	public static void removeMedItem(int position) {
 		MedReminderEntity med = listMeds.get(position);
 		// remove reminder via TaskHandler
 		TaskHandler.cancelMedAlarm(med);
@@ -122,18 +154,10 @@ public class MedReminderFragment extends Fragment {
 		adapterMeds.notifyDataSetChanged();
 	}
 
-	// helper methode for adding a med item
-	public void addMedItem(MedReminderEntity med) {
-		// save entity in DB
-		med.save();
-		// add to med list
-		listMeds.add(med);
-		// sort after remind time with custom comparator
-		Collections.sort(listMeds, new MedReminderEntity.medComparator());
-		// set reminder via TaskHandler
-		TaskHandler.setMedAlarm(med);
-		// update list view via adapter
-		adapterMeds.notifyDataSetChanged();
+	// helper methode for editing a med item
+	public static void editMedItem(int position, MedReminderEntity med) {
+		removeMedItem(position);
+		addMedItem(med);
 	}
 
 	// custom data adapter for the list view
@@ -174,109 +198,6 @@ public class MedReminderFragment extends Fragment {
 					med.remind_minute));
 
 			return convertView;
-		}
-	}
-
-	// med reminder add dialog
-	@SuppressLint("InflateParams")
-	public class MedReminderAddDialog extends DialogFragment {
-
-		@Override
-		public Dialog onCreateDialog(Bundle savedInstanceState) {
-			super.onCreateDialog(savedInstanceState);
-			setRetainInstance(true);
-
-			AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
-			LayoutInflater inflater = getActivity().getLayoutInflater();
-
-			final View view = inflater.inflate(
-					R.layout.dialog_med_reminder_add, null);
-			TimePicker input_med_time = (TimePicker) view
-					.findViewById(R.id.add_med_time);
-			// adapt time picker to user locale
-			input_med_time.setIs24HourView(DateFormat
-					.is24HourFormat(getActivity()));
-			builder.setView(view);
-
-			builder.setTitle(R.string.med_reminder_add_title)
-					// ok button handling
-					.setPositiveButton(android.R.string.ok,
-							new DialogInterface.OnClickListener() {
-								public void onClick(DialogInterface dialog,
-										int id) {
-									// get all input elements
-									EditText input_med_name = (EditText) view
-											.findViewById(R.id.add_med_name);
-									EditText input_med_dose = (EditText) view
-											.findViewById(R.id.add_med_dose);
-									TimePicker input_med_time = (TimePicker) view
-											.findViewById(R.id.add_med_time);
-
-									// read user input
-									String med_name = input_med_name.getText()
-											.toString().trim();
-									String med_dose = input_med_dose.getText()
-											.toString().trim();
-
-									// check input (.isEquals is API level 9)
-									if (med_name.equals("")) {
-										Toast.makeText(
-												getActivity()
-														.getApplicationContext(),
-												R.string.med_reminder_add_name_required,
-												Toast.LENGTH_SHORT).show();
-										return;
-									}
-									if (med_dose.equals("")) {
-										Toast.makeText(
-												getActivity()
-														.getApplicationContext(),
-												R.string.med_reminder_add_dose_required,
-												Toast.LENGTH_SHORT).show();
-										return;
-									}
-
-									// parse dose from string
-									int med_dose_int = 0;
-									try {
-										med_dose_int = Integer
-												.parseInt(med_dose);
-									} catch (NumberFormatException e) {
-										Log.w("MedReminderAddDialog:PositiveButton",
-												"can't parse int from string "
-														+ med_dose);
-									}
-
-									// create new MedReminderEntity
-									MedReminderEntity med = new MedReminderEntity(
-											med_name, med_dose_int,
-											input_med_time.getCurrentHour(),
-											input_med_time.getCurrentMinute());
-
-									// add to list and DB
-									addMedItem(med);
-								}
-							})
-					// cancel button handling
-					.setNegativeButton(android.R.string.cancel,
-							new DialogInterface.OnClickListener() {
-								public void onClick(DialogInterface dialog,
-										int id) {
-									// do nothing
-								}
-							});
-			// create the AlertDialog object and return it
-			return builder.create();
-		}
-
-		// handling of fragment recreation bug in support library
-		// see https://code.google.com/p/android/issues/detail?id=17423
-		// e.g. retains the dialog on rotations
-		@Override
-		public void onDestroyView() {
-			if (getDialog() != null && getRetainInstance())
-				getDialog().setDismissMessage(null);
-			super.onDestroyView();
 		}
 	}
 }
